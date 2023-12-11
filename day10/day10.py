8d5437cedd73c5ab32f82f450323e056816e4bbb
Hristo <sandevh@gmail.com>
2023-12-11 16:16:33 -0500

    day 10

--- day10/day10.py
+++ day10/day10.py
@@ -9,7 +9,7 @@ dir_map = {
     '-': {(0,1), (0,-1)},
     'L': {(-1,0), (0,1)},
     'F': {(1,0), (0,1)},
-    'J': {(-1,0), (-1,0)},
+    'J': {(-1,0), (0,-1)},
     '7': {(1,0), (0,-1)},
     'S': {(-1,0), (0,-1), (1,0), (0,1)},
     '.': {(0,0), (0,0)},
@@ -20,35 +20,43 @@ def bfs(pos):
     queue = deque([pos])
     longest = -1
     while len(queue) > 0:
+        increment = 0
         n = len(queue)
-        longest += 1
+        print(longest, list(map(lambda x: matrix[x[0]][x[1]], queue)))
         for _ in range(n):
             cur = queue.popleft()
             if cur in visited: continue
+            increment = 1
             visited.add(cur)
-            for next in get_valid(cur):
-                queue.append(next)
+            for nxt in get_valid(cur):
+                queue.append(nxt)
+        longest += increment
     return longest
 
 def get_valid(cur):
-    deltas = [(0,1), (0,-1), (1,0), (-1,0)]
+    i_c, j_c = cur
+    deltas = {(0,1), (0,-1), (1,0), (-1,0)}.intersection(dir_map[matrix[i_c][j_c]])
+    print(deltas)
     valid = []
     for delta in deltas:
         dest = tuple(sum(x) for x in zip(cur,delta))
+        i_d, j_c = dest
         if 0 <= dest[0] < len(matrix) and\
             0 <= dest[1] < len(matrix[0]) and\
-                is_valid_connection(cur, dest):
+                (dest[0], dest[1]) not in visited and\
+                    delta in set(map(lambda x: tuple([-1*i for i in x]), dir_map[matrix[i_d][j_c]])):
             valid.append(dest)
     return valid
         
-def is_valid_connection(cur, next):
+def is_valid_connection(cur, nxt):
     # print("bruh", cur, next)
     i_c, j_c = cur
-    i_n, j_n = next
-    cur, next = dir_map[matrix[i_c][j_c]], dir_map[matrix[i_n][j_n]]
-    cur = set(map(lambda x: tuple([-1*i for i in x]), cur))
+    i_n, j_n = nxt 
+    cur, nxt = dir_map[matrix[i_c][j_c]], dir_map[matrix[i_n][j_n]]
+    nxt = set(map(lambda x: tuple([-1*i for i in x]), nxt))
+    print(matrix[i_c][j_c], matrix[i_n][j_n], cur, nxt)
     # print(cur, next)
-    return len(cur.intersection(next)) > 0 
+    return len(cur.intersection(nxt)) > 0 
 
 for i, _ in enumerate(matrix):
     for j, c in enumerate(matrix[i]):
