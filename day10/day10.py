f6e51aa0d96e2be6c3e13646bf90536f1bb06331
Hristo <sandevh@gmail.com>
2023-12-12 22:39:57 -0500

    day 10 plz dont judge code

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,6 @@
-from functools import partial
 from collections import deque
+from pprint import pprint
+from functools import reduce
 with open("input.txt") as f:
     matrix = f.read().split("\n")
 
@@ -15,6 +16,8 @@ dir_map = {
     '.': {(0,0), (0,0)},
 }
 
+part_of_cycle = set()
+
 def bfs(pos):
     global visited, matrix
     queue = deque([pos])
@@ -22,10 +25,10 @@ def bfs(pos):
     while len(queue) > 0:
         increment = 0
         n = len(queue)
-        print(longest, list(map(lambda x: matrix[x[0]][x[1]], queue)))
         for _ in range(n):
             cur = queue.popleft()
             if cur in visited: continue
+            part_of_cycle.add(cur)
             increment = 1
             visited.add(cur)
             for nxt in get_valid(cur):
@@ -36,7 +39,6 @@ def bfs(pos):
 def get_valid(cur):
     i_c, j_c = cur
     deltas = {(0,1), (0,-1), (1,0), (-1,0)}.intersection(dir_map[matrix[i_c][j_c]])
-    print(deltas)
     valid = []
     for delta in deltas:
         dest = tuple(sum(x) for x in zip(cur,delta))
@@ -47,20 +49,85 @@ def get_valid(cur):
                     delta in set(map(lambda x: tuple([-1*i for i in x]), dir_map[matrix[i_d][j_c]])):
             valid.append(dest)
     return valid
-        
-def is_valid_connection(cur, nxt):
-    # print("bruh", cur, next)
-    i_c, j_c = cur
-    i_n, j_n = nxt 
-    cur, nxt = dir_map[matrix[i_c][j_c]], dir_map[matrix[i_n][j_n]]
-    nxt = set(map(lambda x: tuple([-1*i for i in x]), nxt))
-    print(matrix[i_c][j_c], matrix[i_n][j_n], cur, nxt)
-    # print(cur, next)
-    return len(cur.intersection(nxt)) > 0 
 
+def upscale(matrix):
+    new_matrix = []
+    for row in matrix:
+        row = reduce(lambda x,y : x+y, map(lambda x: [x, '.'], row), [])
+        new_matrix.append(row)
+        new_matrix.append(['.' for _ in range(len(row))])
+    marked = set()
+    for i,row in enumerate(new_matrix):
+        if i % 2 == 1: continue
+        for j,c in enumerate(row):
+            if j % 2 == 1: continue
+            dirs = dir_map[c]
+            for d in dirs:
+                if d[0] != 0: #vertical bar
+                    if (i+d[0], j) not in marked: new_matrix[i+d[0]][j] = '|'
+                    marked.add((i+d[0], j))
+                elif d[1] != 0: #horizontal bar
+                    if (i, j+d[1]) not in marked: new_matrix[i][j+d[1]] = '-'
+                    marked.add((i, j+d[1]))
+    
+    return new_matrix
+
+matrix = upscale(matrix)
+# pprint(matrix)
+
+filled = set()
+
+def fill_bfs(cur, symbol):
+    global matrix
+    queue = deque()
+    queue.append(cur) 
+    while len(queue) > 0:
+        n = len(queue)
+        for _ in range(n):
+            cur = queue.popleft()
+            i_c, j_c = cur
+            if cur in part_of_cycle or cur in filled: 
+                continue
+            matrix[i_c][j_c] = symbol
+            filled.add(cur)
+            for d in [(0,1), (1,0), (0,-1), (-1,0)]:
+                if 0 <= i_c + d[0] < len(matrix) and\
+                    0 <= j_c + d[1] < len(matrix[0]):
+                        queue.append((i_c + d[0], j_c + d[1]))
+            
+
+#find cycle 
 for i, _ in enumerate(matrix):
     for j, c in enumerate(matrix[i]):
         pos = (i,j)
         if c == 'S':
-            print(bfs(pos))
-            exit(0)
+            bfs(pos) 
+
+# fill the outside first 
+for i in range(len(matrix[0])):
+    fill_bfs((0,i), 'O')
+    fill_bfs((len(matrix)-1, i), 'O')
+for i in range(len(matrix)):
+    fill_bfs((i,0), '0')
+    fill_bfs((i,len(matrix[0])-1), '0')
+
+
+# fill inside of cycle
+for coord in part_of_cycle:
+    i,j = coord
+    for d in [(0,1), (1,0), (0,-1), (-1,0), (-1,-1), (-1,1), (1,-1), (1,1)]:
+        if 0 <= i + d[0] < len(matrix) and\
+            0 <= j + d[1] < len(matrix[0]):
+                fill_bfs((i+d[0], j+d[1]), 'I')
+
+# count I instances on strictly even tuples (tuples where all numbers are even)
+count_i = 0
+for i, _ in enumerate(matrix):
+    if i % 2 == 1: continue
+    for j, c in enumerate(matrix[i]):
+        if j % 2 == 1: continue
+        count_i += 1 if matrix[i][j] == 'I' else 0
+
+for row in matrix:
+    print(row)
+print(count_i)
